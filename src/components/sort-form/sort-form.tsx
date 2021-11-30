634916841eb6dfc087df135bb8df27b0889d7f55
vasilich99 <oleg_kovtkov@mail.ru>
2021-11-30 17:32:59 +0300

    chore: created an sort-form component from html with radio-buttons

---
+++ src/components/sort-form/sort-form.tsx
@@ -0,0 +1,35 @@
+import classNames from 'classnames';
+import InputWrapper from '../input-wrapper/input-wrapper';
+
+const SortForm = () => {
+  return (
+    <section className={classNames('sort-form', 'weather-content__sort')}>
+      <h2 className="visually-hidden">Форма сортировки</h2>
+      <form action="#" method="GET">
+        <div className="sort-form__group">
+          <InputWrapper
+            className="sort-form__input-wrapper"
+            type="radio"
+            id="alphabet-sort"
+            name="alphabet-sort"
+            value="alphabet-sort"
+            label="Сортировка по алфавиту"
+            iconName="arrow-down"
+            checked
+          />
+          <InputWrapper
+            className="sort-form__input-wrapper"
+            type="radio"
+            id="alphabet-sort-reverse"
+            name="alphabet-sort"
+            value="alphabet-sort-reverse"
+            label="Сортировка по алфавиту в обратном направлении"
+            iconName="arrow-up"
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SortForm;
