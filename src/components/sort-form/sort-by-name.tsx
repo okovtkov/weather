2daf27941f08578d7864fd1c751146b069af0aff
vasilich99 <oleg_kovtkov@mail.ru>
2021-11-30 18:33:35 +0300

    feat: created a sort-by-name component

---
+++ src/components/sort-form/sort-by-name.tsx
@@ -0,0 +1,29 @@
+import InputWrapper from '../input-wrapper/input-wrapper';
+
+const SortByName = () => {
+  return (
+    <>
+      <InputWrapper
+        className="sort-form__input-wrapper"
+        type="radio"
+        id="alphabet-sort"
+        name="alphabet-sort"
+        value="alphabet-sort"
+        label="Сортировка по алфавиту"
+        iconName="arrow-down"
+        checked
+      />
+      <InputWrapper
+        className="sort-form__input-wrapper"
+        type="radio"
+        id="alphabet-sort-reverse"
+        name="alphabet-sort"
+        value="alphabet-sort-reverse"
+        label="Сортировка по алфавиту в обратном направлении"
+        iconName="arrow-up"
+      />
+    </>
+  );
+};
+
+export default SortByName;
