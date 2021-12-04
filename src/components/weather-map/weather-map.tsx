e7fdb989d52d98b944b2a400b144f2c5957979a3
vasilich99 <oleg_kovtkov@mail.ru>
2021-12-05 02:06:24 +0300

    feat: added a weather-map component

---
+++ src/components/weather-map/weather-map.tsx
@@ -0,0 +1,11 @@
+import './weather-map.scss';
+
+const WeatherMap = () => {
+  return (
+    <div className="weather-map">
+      <img src="/images/map.jpg" alt="карта" />
+    </div>
+  );
+};
+
+export default WeatherMap;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import WeatherContent from './components/weather-content/weather-content';
+import WeatherMap from './components/weather-map/weather-map';
 
 ReactDOM.render(
   <StrictMode>
-    <WeatherContent />
+    <div className="weather-app">
+      <WeatherContent />
+      <WeatherMap />
+    </div>
   </StrictMode>,
   document.getElementById('root')
 );
