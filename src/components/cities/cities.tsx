772675230d909df9041dd7d737c062c982a46224
vasilich99 <oleg_kovtkov@mail.ru>
2021-12-04 17:16:03 +0300

    feat: create cities component

---
+++ src/components/cities/cities.tsx
@@ -0,0 +1,10 @@
+const Cities = () => {
+  return (
+    <section className="cities">
+      <h2 className="visually-hidden">Результаты сортировки</h2>
+      <div className="cities__small-cards">*Small-Cards*</div>
+    </section>
+  );
+};
+
+export default Cities;
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import SortForm from './components/sort-form/sort-form';
+import Cities from './components/cities/cities';
 
 ReactDOM.render(
   <StrictMode>
     <SortForm />
+    <Cities />
   </StrictMode>,
   document.getElementById('root')
 );
