a994dc4dc7b2ef72a2dcae0f520fbefd464e92d0
vasilich99 <oleg_kovtkov@mail.ru>
2021-12-06 23:02:18 +0300

    fix: added help-message in cards component

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,6 +1,7 @@
 import BigCard from '../big-card/big-card';
 import SmallCard from '../small-card/small-card';
 import './cards.scss';
+import '../weather-content/weather-content.scss';
 
 const Cards = () => {
   return (
@@ -28,6 +29,9 @@ const Cards = () => {
         <SmallCard />
       </div>
       <div className="cards__big-cards">
+        <div className="cards__help">
+          Перетащите сюда города, погода в которых вам интересна
+        </div>
         <BigCard />
       </div>
     </section>
