30d02a57561658fce80dc858d76b2c9c4a745e64
vasilich99 <oleg_kovtkov@mail.ru>
2022-01-05 01:00:27 +0300

    feat: add an ability to drag and drop small-card without adding to favorites

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,12 +1,22 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { MouseEvent } from 'react';
 import classNames from 'classnames';
 import './icon.scss';
 
 interface Props {
   name: string;
+  onMouseDown?: (event: MouseEvent<HTMLSpanElement>) => void;
+  onMouseUp?: (event: MouseEvent<HTMLSpanElement>) => void;
 }
 
 const Icon = (props: Props) => {
-  return <span className={classNames('icon', `icon--${props.name}`)} />;
+  return (
+    <span
+      className={classNames('icon', `icon--${props.name}`)}
+      onMouseDown={props.onMouseDown}
+      onMouseUp={props.onMouseUp}
+    />
+  );
 };
 
 export default Icon;
--- src/components/small-card/small-card.tsx
+++ src/components/small-card/small-card.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import { useCallback, useMemo } from 'react';
+/* eslint-disable no-param-reassign */
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import Icon from '../icon/icon';
 import './small-card.scss';
 import { City, Weather } from '../../types';
@@ -11,10 +10,70 @@ interface Props {
   onAddFavourite: (city: City) => void;
 }
 
+interface MouseDownInfo {
+  startLeft: number;
+  startTop: number;
+  startX: number;
+  startY: number;
+}
+
 const SmallCard = (props: Props) => {
-  const clickHandler = useCallback(() => {
-    props.onAddFavourite(props.city);
-  }, [props]);
+  const [draggable, setDraggable] = useState<HTMLElement | null>(null);
+  const [mouseDownInfo, setMouseDownInfo] = useState<MouseDownInfo>({
+    startLeft: 0,
+    startTop: 0,
+    startX: 0,
+    startY: 0,
+  });
+  const city = useRef<HTMLDivElement>(null);
+
+  const mouseMoveHandler = useCallback(
+    (e) => {
+      if (!draggable) return;
+      draggable.style.left = `${
+        mouseDownInfo.startLeft + (e.pageX - mouseDownInfo.startX)
+      }px`;
+      draggable.style.top = `${
+        mouseDownInfo.startTop + (e.pageY - mouseDownInfo.startY)
+      }px`;
+    },
+    [draggable, mouseDownInfo]
+  );
+
+  const mouseDownHandler = useCallback((event) => {
+    const obj = city.current;
+    if (!obj) return;
+
+    if (document.querySelector('.small-card_active')) return;
+    const clone = obj.cloneNode(true) as HTMLElement;
+    obj.classList.add('small-card_draggable');
+    obj.after(clone);
+    clone.classList.add('small-card_active');
+    setDraggable(obj);
+    setMouseDownInfo({
+      startLeft: obj.offsetLeft,
+      startTop: obj.offsetTop,
+      startX: event.pageX,
+      startY: event.pageY,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (draggable) document.addEventListener('mousemove', mouseMoveHandler);
+  }, [draggable, mouseMoveHandler]);
+
+  const mouseUpHandler = useCallback(() => {
+    const clone = document.querySelector('.small-card_active');
+    clone?.remove();
+
+    const draggable = city.current;
+    if (!draggable) return;
+
+    draggable.classList.remove('small-card_draggable');
+    draggable.style.top = `${mouseDownInfo.startTop}px`;
+    draggable.style.left = `${mouseDownInfo.startLeft}px`;
+    document.removeEventListener('mousemove', mouseMoveHandler);
+  }, [mouseDownInfo.startLeft, mouseDownInfo.startTop, mouseMoveHandler]);
 
   const temp = useMemo(() => {
     const temperature = props.weather?.temp;
@@ -22,10 +81,14 @@ const SmallCard = (props: Props) => {
   }, [props.weather]);
 
   return (
-    <div className="small-card" onClick={clickHandler}>
+    <div className="small-card" ref={city}>
       <span className="small-card__city">{props.city.name}</span>
       <span className="small-card__temperature">{temp}°</span>
-      <Icon name="strips-small" />
+      <Icon
+        name="strips-small"
+        onMouseDown={mouseDownHandler}
+        onMouseUp={mouseUpHandler}
+      />
     </div>
   );
 };
