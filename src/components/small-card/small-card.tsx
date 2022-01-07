55d4d10af61d5c6a9f999747fee8c80c449e9d08
vasilich99 <oleg_kovtkov@mail.ru>
2022-01-07 15:56:59 +0300

    feat: create custom hook useDragNDrop

--- src/components/small-card/small-card.tsx
+++ src/components/small-card/small-card.tsx
@@ -1,4 +1,5 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useMemo, useRef } from 'react';
+import useDragNDrop from '../../hooks/dragNdrop';
 import Icon from '../icon/icon';
 import './small-card.scss';
 import { City, Weather } from '../../types';
@@ -10,141 +11,25 @@ interface Props {
   onAddFavourite: (cities: City[]) => void;
 }
 
-interface MouseDownInfo {
-  startLeft: number;
-  startTop: number;
-  startX: number;
-  startY: number;
-}
-
 const SmallCard = (props: Props) => {
-  const [draggable, setDraggable] = useState<HTMLElement | null>(null);
-  const [mouseDownInfo, setMouseDownInfo] = useState<MouseDownInfo>({
-    startLeft: 0,
-    startTop: 0,
-    startX: 0,
-    startY: 0,
+  const card = useRef<HTMLDivElement>(null);
+  useDragNDrop({
+    city: props.city,
+    favourites: props.favourites,
+    onAddFavourite: props.onAddFavourite,
+    card: card.current,
   });
-  const city = useRef<HTMLDivElement>(null);
 
   const temp = useMemo(() => {
     const temperature = props.weather?.temp;
     return temperature && temperature > 0 ? `+${temperature}` : temperature;
   }, [props.weather]);
 
-  const getTarget = useCallback((e) => {
-    const target = document.elementFromPoint(
-      e.clientX,
-      e.clientY
-    ) as HTMLElement;
-    if (!target) return null;
-    return target;
-  }, []);
-
-  const createEmptyCard = useCallback((target) => {
-    const favourite = target.closest('.big-card');
-    const newEmpty = document.createElement('div');
-    const oldEmpty = document.querySelector('.big-card--empty');
-    newEmpty.classList.add('big-card--empty');
-
-    if (target.closest('.big-card__header')) favourite.before(newEmpty);
-    if (target.closest('.big-card__content')) favourite.after(newEmpty);
-    if (target.classList.contains('big-card--empty')) return;
-    if (
-      target.classList.contains('cards__big-cards') ||
-      target.classList.contains('cards__help')
-    ) {
-      const container = document.querySelector('.cards__big-cards');
-      container?.append(newEmpty);
-    }
-
-    if (oldEmpty) oldEmpty.remove();
-  }, []);
-
-  const addFavourite = useCallback(() => {
-    const childrens = document.querySelector('.cards__big-cards')?.children;
-    const empty = document.querySelector('.big-card--empty');
-    if (!childrens || !empty) return;
-    const cards = Array.from(childrens);
-    const index = cards.findIndex((item) =>
-      item.classList.contains('big-card--empty')
-    );
-    if (props.favourites.length === 0) props.onAddFavourite([props.city]);
-    else {
-      const favourites = [...props.favourites];
-      favourites.splice(index, 0, props.city);
-      props.onAddFavourite(favourites);
-    }
-    empty?.remove();
-  }, [props]);
-
-  const mouseMoveHandler = useCallback(
-    (e) => {
-      if (!draggable) return;
-      draggable.style.left = `${
-        mouseDownInfo.startLeft + (e.pageX - mouseDownInfo.startX)
-      }px`;
-      draggable.style.top = `${
-        mouseDownInfo.startTop + (e.pageY - mouseDownInfo.startY)
-      }px`;
-
-      draggable.style.visibility = 'hidden';
-      const target = getTarget(e);
-      if (!target) return;
-      createEmptyCard(target);
-      draggable.style.visibility = 'visible';
-    },
-    [draggable, getTarget, mouseDownInfo, createEmptyCard]
-  );
-
-  const mouseDownHandler = useCallback((event) => {
-    const obj = city.current;
-    const container = document.querySelector('.cards__small-cards');
-    if (!obj || !container) return;
-
-    const clone = obj.cloneNode(true) as HTMLElement;
-    obj.after(clone);
-    clone.classList.add('small-card_active');
-    obj.classList.add('small-card_draggable');
-    obj.style.top = `${clone.getBoundingClientRect().top + window.scrollY}px`;
-    obj.style.left = `${clone.getBoundingClientRect().left + window.scrollX}px`;
-
-    setMouseDownInfo({
-      startLeft: obj.offsetLeft,
-      startTop: obj.offsetTop,
-      startX: event.pageX,
-      startY: event.pageY,
-    });
-    setDraggable(obj);
-  }, []);
-
-  useEffect(() => {
-    if (draggable) document.addEventListener('mousemove', mouseMoveHandler);
-  }, [draggable, mouseMoveHandler]);
-
-  const mouseUpHandler = useCallback(() => {
-    const clone = document.querySelector('.small-card_active');
-    clone?.remove();
-
-    const draggable = city.current;
-    if (!draggable) return;
-
-    draggable.classList.remove('small-card_draggable');
-    draggable.style.top = `${mouseDownInfo.startTop}px`;
-    draggable.style.left = `${mouseDownInfo.startLeft}px`;
-    addFavourite();
-    document.removeEventListener('mousemove', mouseMoveHandler);
-  }, [addFavourite, mouseDownInfo, mouseMoveHandler]);
-
   return (
-    <div className="small-card" ref={city}>
+    <div className="small-card" ref={card}>
       <span className="small-card__city">{props.city.name}</span>
       <span className="small-card__temperature">{temp}°</span>
-      <Icon
-        name="strips-small"
-        onMouseDown={mouseDownHandler}
-        onMouseUp={mouseUpHandler}
-      />
+      <Icon name="strips-small" />
     </div>
   );
 };
---
+++ src/hooks/dragNdrop.ts
@@ -0,0 +1,149 @@
+import { useCallback, useEffect, useState } from 'react';
+import { City } from '../types';
+
+interface Props {
+  city: City;
+  favourites: City[];
+  onAddFavourite: (cities: City[]) => void;
+  card: HTMLElement | null;
+}
+
+interface MouseDownInfo {
+  startLeft: number;
+  startTop: number;
+  startX: number;
+  startY: number;
+}
+
+export default function useDragNDrop(props: Props) {
+  const [draggable, setDraggable] = useState<HTMLElement | null>(null);
+  const [mouseDownInfo, setMouseDownInfo] = useState<MouseDownInfo>({
+    startLeft: 0,
+    startTop: 0,
+    startX: 0,
+    startY: 0,
+  });
+
+  const getTarget = useCallback((e) => {
+    const target = document.elementFromPoint(
+      e.clientX,
+      e.clientY
+    ) as HTMLElement;
+    if (!target) return null;
+    return target;
+  }, []);
+
+  const createEmptyCard = useCallback((target) => {
+    const favourite = target.closest('.big-card');
+    const newEmpty = document.createElement('div');
+    const oldEmpty = document.querySelector('.big-card--empty');
+    newEmpty.classList.add('big-card--empty');
+
+    if (target.closest('.big-card__header')) favourite.before(newEmpty);
+    if (target.closest('.big-card__content')) favourite.after(newEmpty);
+    if (target.classList.contains('big-card--empty')) return;
+    if (
+      target.classList.contains('cards__big-cards') ||
+      target.classList.contains('cards__help')
+    ) {
+      const container = document.querySelector('.cards__big-cards');
+      container?.append(newEmpty);
+    }
+
+    if (oldEmpty) oldEmpty.remove();
+  }, []);
+
+  const addFavourite = useCallback(() => {
+    const childrens = document.querySelector('.cards__big-cards')?.children;
+    const empty = document.querySelector('.big-card--empty');
+    if (!childrens || !empty) return;
+    const cards = Array.from(childrens);
+    const index = cards.findIndex((item) =>
+      item.classList.contains('big-card--empty')
+    );
+    if (props.favourites.length === 0) props.onAddFavourite([props.city]);
+    else {
+      const favourites = [...props.favourites];
+      favourites.splice(index, 0, props.city);
+      props.onAddFavourite(favourites);
+    }
+    empty?.remove();
+  }, [props]);
+
+  const mouseMoveHandler = useCallback(
+    (e) => {
+      if (!draggable) return;
+      draggable.style.left = `${
+        mouseDownInfo.startLeft + (e.pageX - mouseDownInfo.startX)
+      }px`;
+      draggable.style.top = `${
+        mouseDownInfo.startTop + (e.pageY - mouseDownInfo.startY)
+      }px`;
+
+      draggable.style.visibility = 'hidden';
+      const target = getTarget(e);
+      if (!target) return;
+      createEmptyCard(target);
+      draggable.style.visibility = 'visible';
+    },
+    [draggable, getTarget, mouseDownInfo, createEmptyCard]
+  );
+
+  const mouseDownHandler = useCallback(
+    (event) => {
+      const obj = props.card;
+      const container = document.querySelector('.cards__small-cards');
+      if (!obj || !container) return;
+
+      const clone = obj.cloneNode(true) as HTMLElement;
+      obj.after(clone);
+      clone.classList.add('small-card_active');
+      obj.classList.add('small-card_draggable');
+      obj.style.top = `${clone.getBoundingClientRect().top + window.scrollY}px`;
+      obj.style.left = `${
+        clone.getBoundingClientRect().left + window.scrollX
+      }px`;
+
+      setMouseDownInfo({
+        startLeft: obj.offsetLeft,
+        startTop: obj.offsetTop,
+        startX: event.pageX,
+        startY: event.pageY,
+      });
+      setDraggable(obj);
+    },
+    [props.card]
+  );
+
+  const mouseUpHandler = useCallback(() => {
+    const clone = document.querySelector('.small-card_active');
+    clone?.remove();
+
+    const draggable = props.card;
+    if (!draggable) return;
+
+    draggable.classList.remove('small-card_draggable');
+    draggable.style.top = `${mouseDownInfo.startTop}px`;
+    draggable.style.left = `${mouseDownInfo.startLeft}px`;
+    addFavourite();
+    document.removeEventListener('mousemove', mouseMoveHandler);
+  }, [
+    addFavourite,
+    mouseDownInfo.startLeft,
+    mouseDownInfo.startTop,
+    mouseMoveHandler,
+    props.card,
+  ]);
+
+  useEffect(() => {
+    props.card?.addEventListener('mousedown', mouseDownHandler);
+    props.card?.addEventListener('mouseup', mouseUpHandler);
+    if (draggable) document.addEventListener('mousemove', mouseMoveHandler);
+  }, [
+    draggable,
+    mouseDownHandler,
+    mouseMoveHandler,
+    mouseUpHandler,
+    props.card,
+  ]);
+}
